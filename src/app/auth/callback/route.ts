66b2a801db7010a6915de9423a1a826b5d051f35
codenilson <fcodenilson@gmail.com>
2026-04-05 12:39:06 -0300

    feat: remove error handling and logging from authentication callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,17 +8,9 @@ export async function GET(request: Request) {
 
   if (code) {
     const supabase = await createSupabaseServerClient()
-    
-    // Capturamos o erro aqui!
+
     const { error } = await supabase.auth.exchangeCodeForSession(code)
-    
-    if (error) {
-      console.error("🚨 ERRO GRAVE NO LOGIN:", error.message)
-      // Opcional: Redirecionar para uma página de erro do seu site
-      // return NextResponse.redirect(`${requestUrl.origin}/login?error=true`)
-    } else {
-      console.log("✅ Usuário logado com sucesso. Cookies gerados!")
-    }
+
   }
 
   const redirectUrl = new URL(env.NEXT_PUBLIC_APP_URL ?? requestUrl.origin)
