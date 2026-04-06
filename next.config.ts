6eea925d27eea1d05ce3827c4c29df2957095bbc
codenilson <fcodenilson@gmail.com>
2026-04-05 21:50:01 -0300

    feat: add allowedDevOrigins configuration for image support

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Adicione a configuração allowedDevOrigins aqui
+  allowedDevOrigins: ['127.0.0.1'],
+  
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
+        port: '54321',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '1277.0.0.1',
         port: '54321',
         pathname: '/storage/v1/object/public/**',
       },
