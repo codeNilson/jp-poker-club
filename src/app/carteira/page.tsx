9ee9f2dce1014ad45e0d66e32bdc05a136277a51
codenilson <fcodenilson@gmail.com>
2026-04-25 07:23:42 -0300

    feat: add Carteira and Perfil components with welcome messages

---
+++ src/app/carteira/page.tsx
@@ -0,0 +1,8 @@
+export default function Carteira() {
+  return (
+    <div>
+      <h1>Perfil</h1>
+      <p>Bem-vindo a sua carteira!</p>
+    </div>
+  );
+}
---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,8 @@
+export default function Perfil() {
+  return (
+    <div>
+      <h1>Perfil</h1>
+      <p>Bem-vindo ao seu perfil!</p>
+    </div>
+  );
+}
